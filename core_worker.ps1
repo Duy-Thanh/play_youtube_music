1501619339b02998bb047c05fbcb990d7d6ff9d2
ThanhDN <36120800+Duy-Thanh@users.noreply.github.com>
2026-06-23 23:17:00 +0700

    Add files via upload

--- core_worker.ps1
+++ core_worker.ps1
@@ -1,4 +1,4 @@
 param (
     [string]$Link
 )
-cmd.exe /c "`"%USERPROFILE%\scoop\apps\yt-dlp\current\yt-dlp.exe`" --cookies `"%USERPROFILE%\c1.txt`" -f ba -o - -- `"$Link`" | `"%USERPROFILE%\scoop\apps\ffmpeg\current\bin\ffmpeg.exe`" -re -i - -c:a copy -f mpegts udp://127.0.0.1:1234?pkt_size=1316"
+cmd.exe /c "`"%USERPROFILE%\scoop\apps\yt-dlp\current\yt-dlp.exe`" --cookies `"%USERPROFILE%\c1.txt`" -f ba -o - -- `"$Link`" | `"%USERPROFILE%\scoop\apps\ffmpeg\current\bin\ffmpeg.exe`" -re -i - -c:a copy -max_delay 500000 -pes_payload_size 0 -f mpegts udp://127.0.0.1:1234?pkt_size=1316"
